a8d30f47e299679a9dd5fab57385c6662fba4ca6
Michael Vorburger <mike@vorburger.ch>
2018-11-29 20:34:15 +0100

    add build-COE.sh

---
+++ build-COE.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+set -e
+
+if [ $# -ne 1 ]; then
+  echo "USAGE: $0 <COE_DIR>"
+  exit -1
+fi
+COE_DIR=$1
+set -x
+
+# https://github.com/opendaylight/coe/blob/master/docs/setting-up-coe-dev-environment.rst#building-coe-watcher-and-cni-plugin
+
+pushd .
+
+cd $COE_DIR/watcher
+dep ensure -vendor-only
+go build
+
+cd $COE_DIR/odlCNIPlugin/odlovs-cni
+dep ensure -vendor-only
+go build
+
+popd
