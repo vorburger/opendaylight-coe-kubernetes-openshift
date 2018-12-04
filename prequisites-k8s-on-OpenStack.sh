a486257da5293efa7895598c4f8166d9c58d900b
Michael Vorburger <mike@vorburger.ch>
2018-12-04 17:41:31 +0100

    open ports 6640 for OVSDB & 6653 for OpenFlow

--- prequisites-k8s-on-OpenStack.sh
+++ prequisites-k8s-on-OpenStack.sh
@@ -10,11 +10,19 @@ openstack security group rule create k8s-master --protocol TCP --dst-port 2380:2
 openstack security group rule create k8s-master --protocol TCP --dst-port 10250:10250
 openstack security group rule create k8s-master --protocol TCP --dst-port 10251:10251
 openstack security group rule create k8s-master --protocol TCP --dst-port 10252:10252
-# Port 9090 is for https://cockpit-project.org (HTTPS)
+
+# Port 9090 for https://cockpit-project.org (HTTPS)
 openstack security group rule create k8s-master --protocol TCP --dst-port 9090:9090
+
+# Ports 6640 for OVSDB  &  6653 for OpenFlow
+openstack security group rule create k8s-master --protocol TCP --dst-port 6640:6640 --remote-ip 192.168.0.0/24
+openstack security group rule create k8s-master --protocol TCP --dst-port 6653:6653 --remote-ip 192.168.0.0/24
+
 
 openstack security group create k8s-node
 openstack security group rule create k8s-node --protocol TCP --dst-port 10250:10250
+openstack security group rule create k8s-node --protocol TCP --dst-port 6640:6640 --remote-ip 192.168.0.0/24
+openstack security group rule create k8s-node --protocol TCP --dst-port 6653:6653 --remote-ip 192.168.0.0/24
 
 # TODO Floating IP
 # openstack floating ip create fbf9bcc6-cbaa-4c24-8d56-8010915a6494
