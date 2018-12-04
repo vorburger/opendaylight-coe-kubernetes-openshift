64601631dfadf33b1ef3eda0b3d74ee318fc8009
Michael Vorburger <mike@vorburger.ch>
2018-12-04 21:45:11 +0100

    fix broken get_private_IP() with grep -q

--- utils.sh
+++ utils.sh
@@ -2,14 +2,14 @@
 
 get_private_IP() {
     local NAME=$1
-    until openstack server list --name $NAME | grep 'private='; do sleep 1 ; done
+    until openstack server list --name $NAME | grep -q 'private='; do sleep 1 ; done
     local IP=$(openstack server list --name $NAME -c Networks --format value | sed 's/private=\([0-9.]\+\).*/\1/')
     echo $IP
     # ^^ NB Bash foo - must "echo" not "return" for non-numeric reply.
 }
 get_public_IP() {
     local NAME=$1
-    until openstack server list --name $NAME | grep 'private='; do sleep 1 ; done
+    until openstack server list --name $NAME | grep -q 'private='; do sleep 1 ; done
     until openstack server list --name $NAME -c Networks --format value | grep ', '; do sleep 1 ; done
     local IP=$(openstack server list --name $NAME -c Networks --format value | sed 's/private=\([0-9.]\+\), \([0-9.]\+\)/\2/')
     echo $IP
