0d9170dfdb233f1460e3dd9182beb8cbac3773d6
Michael Vorburger <mike@vorburger.ch>
2018-12-04 21:49:24 +0100

    await VM actually being ACTIVE

--- utils.sh
+++ utils.sh
@@ -3,6 +3,7 @@
 get_private_IP() {
     local NAME=$1
     until openstack server list --name $NAME | grep -q 'private='; do sleep 1 ; done
+    until openstack server show $NAME | grep -q 'ACTIVE'; do sleep 1 ; done
     local IP=$(openstack server list --name $NAME -c Networks --format value | sed 's/private=\([0-9.]\+\).*/\1/')
     echo $IP
     # ^^ NB Bash foo - must "echo" not "return" for non-numeric reply.
@@ -11,6 +12,7 @@ get_public_IP() {
     local NAME=$1
     until openstack server list --name $NAME | grep -q 'private='; do sleep 1 ; done
     until openstack server list --name $NAME -c Networks --format value | grep ', '; do sleep 1 ; done
+    until openstack server show $NAME | grep -q 'ACTIVE'; do sleep 1 ; done
     local IP=$(openstack server list --name $NAME -c Networks --format value | sed 's/private=\([0-9.]\+\), \([0-9.]\+\)/\2/')
     echo $IP
 }
