0caab3085c9a8d45ce1a0cda3b0b0abfdd0743b1
Michael Vorburger <mike@vorburger.ch>
2018-12-04 23:45:35 +0100

    {WIP, NOK} test cross node ping!

--- test.sh
+++ test.sh
@@ -12,15 +12,16 @@ source ./utils.sh
 MASTER_PUBLIC_IP=$(get_public_IP $NAME_PREFIX-master)
 
 scp pods/* fedora@$MASTER_PUBLIC_IP:
-ssh fedora@$MASTER_PUBLIC_IP "kubectl delete -f busybox1.yaml -f busybox2.yaml" || true
-ssh fedora@$MASTER_PUBLIC_IP "kubectl apply -f busybox1.yaml -f busybox2.yaml"
+ssh fedora@$MASTER_PUBLIC_IP "kubectl delete -f busybox-1.1.yaml -f busybox-1.2.yaml -f busybox-2.1.yaml" || true
+ssh fedora@$MASTER_PUBLIC_IP "kubectl apply -f busybox-1.1.yaml -f busybox-1.2.yaml -f busybox-2.1.yaml"
 # TODO await Pod STATUS Running instead of sleep
 sleep 5
 ssh fedora@$MASTER_PUBLIC_IP "kubectl get pods -o wide"
 
-busybox2_IP=$(ssh -t fedora@$MASTER_PUBLIC_IP "kubectl get pod busybox2 --template={{.status.podIP}}")
-ssh fedora@$MASTER_PUBLIC_IP "kubectl exec -it busybox1 -- ping -c 1 -w 1 $busybox2_IP"
+busybox12_IP=$(ssh -t fedora@$MASTER_PUBLIC_IP "kubectl get pod busybox-1.2 --template={{.status.podIP}}")
+ssh fedora@$MASTER_PUBLIC_IP "kubectl exec -it busybox-1.1 -- ping -c 1 -w 1 $busybox12_IP"
 
-# TODO create busybox3 on node2 and ping accross
+busybox21_IP=$(ssh -t fedora@$MASTER_PUBLIC_IP "kubectl get pod busybox-2.1 --template={{.status.podIP}}")
+ssh fedora@$MASTER_PUBLIC_IP "kubectl exec -it busybox-1.1 -- ping -c 1 -w 1 $busybox21_IP"
 
 # TODO deploy a web server and get hello world welcome page via HTTP and using service DNS name instead of IP
