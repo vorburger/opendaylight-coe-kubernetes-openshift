85a1d79fec5306e409a4b5916ff6d74e943991f0
Michael Vorburger <mike@vorburger.ch>
2018-12-05 11:53:18 +0100

    scp -o StrictHostKeyChecking=no

--- OpenShift/setup-OpenShift-on-OpenStack.sh
+++ OpenShift/setup-OpenShift-on-OpenStack.sh
@@ -22,18 +22,16 @@ openstack server add security group $NAME_PREFIX-node2 openshift-node
 
 ANSIBLE_PUBLIC_IP=$(get_public_IP $NAME_PREFIX-ansible)
 ssh-keygen -R $ANSIBLE_PUBLIC_IP || true
-ssh -o StrictHostKeyChecking=no $USER@$ANSIBLE_PUBLIC_IP "echo hello, new VM"
-
-scp {remote-setup-common.sh,remote-setup-ansible.sh} $USER@$ANSIBLE_PUBLIC_IP:
+scp -o StrictHostKeyChecking=no {remote-setup-common.sh,remote-setup-ansible.sh} $USER@$ANSIBLE_PUBLIC_IP:
 ssh $USER@$ANSIBLE_PUBLIC_IP "./remote-setup-common.sh"
 sleep 10
 
 MASTER_PRIVATE_IP=$(get_private_IP $NAME_PREFIX-master)
-ssh $USER@$ANSIBLE_PUBLIC_IP "scp remote-setup-common.sh $USER@$MASTER_PRIVATE_IP: ; ssh $USER@$MASTER_PRIVATE_IP ./remote-setup-common.sh"
+ssh $USER@$ANSIBLE_PUBLIC_IP "scp -o StrictHostKeyChecking=no remote-setup-common.sh $USER@$MASTER_PRIVATE_IP: ; ssh $USER@$MASTER_PRIVATE_IP ./remote-setup-common.sh"
 NODE1_PRIVATE_IP=$(get_private_IP $NAME_PREFIX-node1)
-ssh $USER@$ANSIBLE_PUBLIC_IP "scp remote-setup-common.sh $USER@$NODE1_PRIVATE_IP: ; ssh $USER@$NODE1_PRIVATE_IP ./remote-setup-common.sh"
+ssh $USER@$ANSIBLE_PUBLIC_IP "scp -o StrictHostKeyChecking=no remote-setup-common.sh $USER@$NODE1_PRIVATE_IP: ; ssh $USER@$NODE1_PRIVATE_IP ./remote-setup-common.sh"
 NODE2_PRIVATE_IP=$(get_private_IP $NAME_PREFIX-node2)
-ssh $USER@$ANSIBLE_PUBLIC_IP "scp remote-setup-common.sh $USER@$NODE2_PRIVATE_IP: ; ssh $USER@$NODE2_PRIVATE_IP ./remote-setup-common.sh"
+ssh $USER@$ANSIBLE_PUBLIC_IP "scp -o StrictHostKeyChecking=no remote-setup-common.sh $USER@$NODE2_PRIVATE_IP: ; ssh $USER@$NODE2_PRIVATE_IP ./remote-setup-common.sh"
 
 scp hosts $USER@$ANSIBLE_PUBLIC_IP:
 ssh $USER@$ANSIBLE_PUBLIC_IP "sudo cp hosts /etc/ansible/"
