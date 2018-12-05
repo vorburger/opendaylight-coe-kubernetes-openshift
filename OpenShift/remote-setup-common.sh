f3599b8c724ad1fee8bb88ac5f119996f3b28ec0
Michael Vorburger <mike@vorburger.ch>
2018-12-05 01:25:39 +0100

    fix "No module named yaml" during OpenShift installation (WIP)

--- OpenShift/remote-setup-common.sh
+++ OpenShift/remote-setup-common.sh
@@ -2,6 +2,7 @@
 set -ex
 
 # TODO Cockpit? Later..
-sudo dnf install -y wget git net-tools bind-utils yum-utils iptables-services bridge-utils bash-completion kexec-tools sos psacct
+# NB We install "ansible pyOpenSSL" on all hosts, not just the ansible jump host, to work around playbooks/prerequisites.yml failure "No module named yaml"
+sudo dnf install -y ansible pyOpenSSL wget git net-tools bind-utils yum-utils iptables-services bridge-utils bash-completion kexec-tools sos psacct
 sudo dnf -y update
 sudo reboot now
