1069a777b5f9d24d546068c4d6410d49f8d9ed14
Michael Vorburger <mike@vorburger.ch>
2018-12-05 11:45:05 +0100

    use CentOS 7.6.1810 instead of Fedora 28 for OpenShift installation
    Hope that this will help with some Python 3 vs 2 problem which "ansible-playbook playbooks/prerequisites.yml" reports.
    
    And even if it doesn't it's perhaps still a good idea anyway.

--- OpenShift/remote-setup-common.sh
+++ OpenShift/remote-setup-common.sh
@@ -3,6 +3,6 @@ set -eux
 
 # TODO Cockpit? Later..
 # NB We install "ansible pyOpenSSL" on all hosts, not just the ansible jump host, to work around playbooks/prerequisites.yml failure "No module named yaml"
-sudo dnf install -y ansible pyOpenSSL wget git net-tools bind-utils yum-utils iptables-services bridge-utils bash-completion kexec-tools sos psacct
-sudo dnf -y update
+sudo yum install -y ansible pyOpenSSL wget git net-tools bind-utils yum-utils iptables-services bridge-utils bash-completion kexec-tools sos psacct
+sudo yum -y update
 sudo reboot now
--- OpenShift/setup-OpenShift-on-OpenStack.sh
+++ OpenShift/setup-OpenShift-on-OpenStack.sh
@@ -1,34 +1,40 @@
 #!/bin/bash
-set -e
+set -eu
 
 NAME_PREFIX=openshift
+USER=centos
 
 set -x
-source ./utils.sh
+source ../utils.sh
 
-openstack server create --flavor m1.small --image Fedora-Cloud-Base-28-1.1.x86_64 --security-group ssh --key-name laptop $NAME_PREFIX-ansible
+openstack server create --flavor m1.small --image CentOS-7-x86_64-GenericCloud-1804_02 --security-group ssh --key-name laptop $NAME_PREFIX-ansible
+# This forces awaiting the VM being up
+ANSIBLE_PRIVATE_IP=$(get_private_IP $NAME_PREFIX-ansible)
 openstack server add floating ip $NAME_PREFIX-ansible 38.145.33.97
 
-openstack server create --flavor m1.xlarge --image Fedora-Cloud-Base-28-1.1.x86_64 --security-group ssh --key-name laptop $NAME_PREFIX-master
-openstack server create --flavor m1.large --image Fedora-Cloud-Base-28-1.1.x86_64 --security-group ssh --key-name laptop $NAME_PREFIX-node1
-openstack server create --flavor m1.large --image Fedora-Cloud-Base-28-1.1.x86_64 --security-group ssh --key-name laptop $NAME_PREFIX-node2
+openstack server create --flavor m1.xlarge --image CentOS-7-x86_64-GenericCloud-1804_02 --security-group ssh --key-name laptop $NAME_PREFIX-master
+openstack server create --flavor m1.large --image CentOS-7-x86_64-GenericCloud-1804_02 --security-group ssh --key-name laptop $NAME_PREFIX-node1
+openstack server create --flavor m1.large --image CentOS-7-x86_64-GenericCloud-1804_02 --security-group ssh --key-name laptop $NAME_PREFIX-node2
 
 openstack server add security group $NAME_PREFIX-master openshift-master
 openstack server add security group $NAME_PREFIX-node1 openshift-node
 openstack server add security group $NAME_PREFIX-node2 openshift-node
 
 ANSIBLE_PUBLIC_IP=$(get_public_IP $NAME_PREFIX-ansible)
-scp {remote-setup-common.sh,remote-setup-ansible.sh} fedora@$ANSIBLE_PUBLIC_IP:
-ssh fedora@$ANSIBLE_PUBLIC_IP "./remote-setup-common.sh"
+ssh-keygen -R $ANSIBLE_PUBLIC_IP || true
+ssh -o StrictHostKeyChecking=no $USER@$ANSIBLE_PUBLIC_IP "echo hello, new VM"
+
+scp {remote-setup-common.sh,remote-setup-ansible.sh} $USER@$ANSIBLE_PUBLIC_IP:
+ssh $USER@$ANSIBLE_PUBLIC_IP "./remote-setup-common.sh"
 sleep 10
 
 MASTER_PRIVATE_IP=$(get_private_IP $NAME_PREFIX-master)
-ssh fedora@$ANSIBLE_PUBLIC_IP "scp remote-setup-common.sh fedora@$MASTER_PRIVATE_IP: ; ssh fedora@$MASTER_PRIVATE_IP ./remote-setup-common.sh"
+ssh $USER@$ANSIBLE_PUBLIC_IP "scp remote-setup-common.sh $USER@$MASTER_PRIVATE_IP: ; ssh $USER@$MASTER_PRIVATE_IP ./remote-setup-common.sh"
 NODE1_PRIVATE_IP=$(get_private_IP $NAME_PREFIX-node1)
-ssh fedora@$ANSIBLE_PUBLIC_IP "scp remote-setup-common.sh fedora@$NODE1_PRIVATE_IP: ; ssh fedora@$NODE1_PRIVATE_IP ./remote-setup-common.sh"
+ssh $USER@$ANSIBLE_PUBLIC_IP "scp remote-setup-common.sh $USER@$NODE1_PRIVATE_IP: ; ssh $USER@$NODE1_PRIVATE_IP ./remote-setup-common.sh"
 NODE2_PRIVATE_IP=$(get_private_IP $NAME_PREFIX-node2)
-ssh fedora@$ANSIBLE_PUBLIC_IP "scp remote-setup-common.sh fedora@$NODE2_PRIVATE_IP: ; ssh fedora@$NODE2_PRIVATE_IP ./remote-setup-common.sh"
+ssh $USER@$ANSIBLE_PUBLIC_IP "scp remote-setup-common.sh $USER@$NODE2_PRIVATE_IP: ; ssh $USER@$NODE2_PRIVATE_IP ./remote-setup-common.sh"
 
-scp hosts fedora@$ANSIBLE_PUBLIC_IP:
-ssh fedora@$ANSIBLE_PUBLIC_IP "sudo cp hosts /etc/ansible/"
-ssh fedora@$ANSIBLE_PUBLIC_IP "./remote-setup-ansible.sh"
+scp hosts $USER@$ANSIBLE_PUBLIC_IP:
+ssh $USER@$ANSIBLE_PUBLIC_IP "sudo cp hosts /etc/ansible/"
+ssh $USER@$ANSIBLE_PUBLIC_IP "./remote-setup-ansible.sh"
