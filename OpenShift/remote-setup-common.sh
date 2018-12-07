d9ce285b790d2deb1043ebbd835b92fac45a2b3e
Michael Vorburger <mike@vorburger.ch>
2018-12-07 14:37:48 +0100

    install Ansible from EPEL (fixes #2)

--- OpenShift/remote-setup-common.sh
+++ OpenShift/remote-setup-common.sh
@@ -2,7 +2,13 @@
 set -eux
 
 # TODO Cockpit? Later..
+
 # NB We install "ansible pyOpenSSL" on all hosts, not just the ansible jump host, to work around playbooks/prerequisites.yml failure "No module named yaml"
-sudo yum install -y ansible pyOpenSSL wget git net-tools bind-utils yum-utils iptables-services bridge-utils bash-completion kexec-tools sos psacct
+# BUT we need Ansible from EPEL, not CentOS (see https://github.com/vorburger/opendaylight-coe-kubernetes-openshift/issues/2)
+sudo yum -y install https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
+sudo sed -i -e "s/^enabled=1/enabled=0/" /etc/yum.repos.d/epel.repo
+sudo yum -y --enablerepo=epel install ansible pyOpenSSL
+
+sudo yum install -y wget git net-tools bind-utils yum-utils iptables-services bridge-utils bash-completion kexec-tools sos psacct
 sudo yum -y update
 sudo reboot now
