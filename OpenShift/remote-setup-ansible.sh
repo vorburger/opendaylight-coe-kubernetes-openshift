06596fcd454047e097f6778f490cd009d0287913
Michael Vorburger <mike@vorburger.ch>
2018-12-12 21:51:31 +0100

    add "documentation by echo", for now (#6)

--- OpenShift/remote-setup-ansible.sh
+++ OpenShift/remote-setup-ansible.sh
@@ -8,3 +8,7 @@ git checkout release-3.11
 sudo cp ../hosts /etc/ansible/
 ansible-playbook playbooks/prerequisites.yml | tee ../prerequisites.log
 ansible-playbook playbooks/deploy_cluster.yml | tee ../deploy_cluster.log
+
+# TODO Automate this...
+echo "If the deployment was successful, you should 'sudo reboot now' the master; see https://github.com/vorburger/opendaylight-coe-kubernetes-openshift/issues/6"
+echo "Now run 'oc get nodes -o wide' on the master to Verify the Installation!"
