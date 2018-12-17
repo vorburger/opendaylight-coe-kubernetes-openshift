2ad376d77032adba13879282ed3876bf4d7cc668
Michael Vorburger <mike@vorburger.ch>
2018-12-17 19:33:06 +0100

    fix get_private_IP() & get_public_IP() in utils.sh
    because e.g. "openstack server list --name coe-node"
    would return both 'coe-node' & 'coe-node2' (or node1
    woud return both node1 and node11), because --name
    is a Reg Exp - so we use \$ to terminate it.

--- utils.sh
+++ utils.sh
@@ -2,17 +2,17 @@
 
 get_private_IP() {
     local NAME=$1
-    until openstack server list --name $NAME | grep -q 'private='; do sleep 1 ; done
+    until openstack server list --name $NAME\$ | grep -q 'private='; do sleep 1 ; done
     until openstack server show $NAME | grep -q 'ACTIVE'; do sleep 1 ; done
-    local IP=$(openstack server list --name $NAME -c Networks --format value | sed 's/private=\([0-9.]\+\).*/\1/')
+    local IP=$(openstack server list --name $NAME\$ -c Networks --format value | sed 's/private=\([0-9.]\+\).*/\1/')
     echo $IP
     # ^^ NB Bash foo - must "echo" not "return" for non-numeric reply.
 }
 get_public_IP() {
     local NAME=$1
-    until openstack server list --name $NAME | grep -q 'private='; do sleep 1 ; done
-    until openstack server list --name $NAME -c Networks --format value | grep -q ', '; do sleep 1 ; done
+    until openstack server list --name $NAME\$ | grep -q 'private='; do sleep 1 ; done
+    until openstack server list --name $NAME\$ -c Networks --format value | grep -q ', '; do sleep 1 ; done
     until openstack server show $NAME | grep -q 'ACTIVE'; do sleep 1 ; done
-    local IP=$(openstack server list --name $NAME -c Networks --format value | sed 's/private=\([0-9.]\+\), \([0-9.]\+\)/\2/')
+    local IP=$(openstack server list --name $NAME\$ -c Networks --format value | sed 's/private=\([0-9.]\+\), \([0-9.]\+\)/\2/')
     echo $IP
 }
