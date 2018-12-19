1929babbd5157704bc45089d6af3f8e118309c0c
Michael Vorburger <mike@vorburger.ch>
2018-12-19 14:05:40 +0100

    clean tun ports as well, not just veth ports

--- remote-clean-ovs-veth-ports.sh
+++ remote-clean-ovs-veth-ports.sh
@@ -2,3 +2,5 @@
 set -eux
 
 sudo ovs-vsctl show | grep "Port \"veth" | awk '{print $2}' | awk -F'"' '{print $2}' | xargs -d '\n' -L 1 sudo ovs-vsctl del-port
+
+sudo ovs-vsctl show | grep "Port \"tun" | awk '{print $2}' | awk -F'"' '{print $2}' | xargs -d '\n' -L 1 sudo ovs-vsctl del-port
